655467c241ca64b4140ed886953d555fc5cacde8
crisleycita <151065786+crisleycita@users.noreply.github.com>
2023-11-16 13:45:38 -0400

    Add files via upload

---
+++ INDEX.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>UNIDAD EDUCATIVA NACIONAL GUAYARAMERIN</title>
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+<link href="css/zoom 2.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css/estilos.css">
+<style type="text/css">
+</style>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<link href="../ENLACES/SpryAssets/zoom 2.css" rel="stylesheet" type="text/css" />
+</head>
+<center><table border="0" cellspacing="10">
+<tr>
+<style type="text/css">
+    * {
+    margin:2px;
+    padding:0px;
+    }
+    #header {
+    margin:auto;
+    width:1200px;
+    font-family:Arial, Helvetica, sans-serif;
+    }
+    ul, ol {
+    list-style:none;
+    }
+    .nav > li {
+    float:left;
+    }
+    .nav li a {
+    background-color:#fff;
+    color:#fff;
+    text-decoration:none;
+    padding:10px 10px;
+    display:block;
+    }
+    .nav li a:hover {
+    background-color:#0C0;
+    }
+    .nav li ul {
+    display:none;
+    position:absolute;
+    min-width:160px;
+    }
+    .nav li:hover > ul {
+    display:block;
+    }
+    .nav li ul li {
+    position:relative;
+    }
+    .nav li ul li ul {
+    right:-200px;
+    top:20px;
+    }
+    .Estilo1 {    color: #0000CD;
+        font-size: 35px;
+    }
+    .Estilo2 {font-size: 12px}
+    .Estilo3 {color: #0000CD}
+    .Estilo4 {
+        color: #000000;
+        font-weight: bold;
+    }
+    .Estilo5 {color: #0000CD}
+    </style>
+<body>
+<img  src="images/fondo.jpg" class="m">
+<CENTER><H1><FONT FACE="algerian"COLOR="#009900">UNIDAD EDUCATIVA NACIONAL GUAYARAMERIN</FONT></H1><br />
+<section class="objet"></section></CENTER>
+<BR>
+ <div id="header">
+    <td>
+      <ul class="nav">
+    <a href="Enlases/REGISTRO USUARIOS.php">
+    <center><img class="zoom" src="images/usuario.png" width="100" height="100" alt="g" /></center>
+    <li><FONT FACE="Arial Black"SIZE="5"COLOR="#000000">NUEVO USUARIO</font></a>
+  </td>
+ <div id="header">
+<td><ul class="nav"><a href="Enlases/REGISTRO DOCENTES.php">
+<center><img class="zoom" src="images/actividades.png" width="100" height="100" alt="g" /></center>
+<li><FONT FACE="Arial Black"SIZE="5"COLOR="#000000">REGISTRO DE DOCENTES</font></a></strong></td>
+<td><ul class="nav"><a href="Enlases/REGISTRO ALUMNOS.php"><center><img class="zoom" src="images/estudiante.png" width="100" height="100" alt="g" /></center>
+<li><FONT FACE="Arial Black"SIZE="5"COLOR="#000000">REGISTRO DE ESTUDIANTES</font></a></strong></td>
+<td><ul class="nav">
+<a href="Enlases/REPORTES.html">
+<center><img class="zoom" src="images/REPORTES.png" width="100" height="100" alt="g" /></center>
+<li><FONT FACE="Arial Black"SIZE="5"COLOR="#000000">REPORTES</font></a></strong></td>
+
+</CENTER>
+</body>
+</html>
---
+++ VALIDACION.html
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>NACIONAL GUAYARAMERIN</title>
+</head>
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+ <link rel="stylesheet" href="css/estilos.css">
+ <script>
+function iniciar(){
+edad=document.getElementById("miedad");
+edad.addEventListener("change", cambiarrango, false);
+document.informacion.addEventListener("invalid",
+validacion, true);
+document.getElementById("enviar").addEventListener("click",
+enviar, false);
+document.informacion.addEventListener("input", controlar,
+false);
+}
+function cambiarrango(){
+var salida=document.getElementById("rango");
+var calc=edad.value-10;
+if(calc<10){
+calc=0;
+edad.value=10;
+}
+salida.innerHTML=calc+' a '+edad.value;
+}
+function validacion(e){
+var elemento=e.target;
+elemento.style.background='#FFDDDD';
+}
+function enviar(){
+var elemento=document.getElementById("usuario");
+var valido=document.informacion.checkValidity();
+if(valido){
+document.informacion.submit();
+}else if(elemento.validity.patternMismatch ||
+elemento.validity.valueMissing){
+alert('el nombre de usuario debe tener mínimo de 3 caracteres');
+}
+}
+function controlar(e){
+var elemento=e.target;
+if(elemento.validity.valid){
+elemento.style.background='#FFFFFF';
+}else{
+elemento.style.background='#FFDDDD';
+}
+}
+window.addEventListener("load", iniciar, false);
+</script>
+<body>
+<img  src="images/fondo.jpg" class="m">
+<CENTER><H1><FONT FACE="algerian"COLOR="#009900">BIENVENIDO A LA PAGINA LICEO NACIONAL GUAYARAMERIN</FONT></H1>
+<section class="objet"></section>
+<BR>
+<section>
+  <form name="informacion" method="get">
+<FONT color="#00FF00">USUARIO:
+<input pattern="[A-Za-z]{3,}" name="usuario" id="usuario"
+maxlength="30" required><BR /><BR />
+EMAIL:
+<input type="CONTRASEÑA" name="CONTRASEÑA" id="CONTRASEÑA" required><BR /><BR />
+RANGO DE EDAD:</FONT>
+<input type="range" name="miedad" id="miedad" min="0"
+max="100" step="10" value="10">
+<output id="rango">0 a 10</output><BR /><BR />
+<input type="button" id="enviar" value="ENVIAR">
+  </form>
+</section>
+<table width="59" border="0">
+  <tr>
+    <td><form id="form1" name="form1" method="post" action="index.html">
+      <input type="submit" name="INGRESAR" id="INGRESAR" value="INGRESAR" />
+    </form></td>
+  </tr>
+</table>
+<style type="text/css">
+
+</body>
+</html>
